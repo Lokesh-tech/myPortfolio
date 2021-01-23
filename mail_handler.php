1ad38a0d6d26f3af8b10c1d338429e31c29a3b72
Lokesh-tech <54676102+Lokesh-tech@users.noreply.github.com>
2021-01-23 12:40:04 +0530

    Add files via upload

---
+++ mail_handler.php
@@ -0,0 +1,20 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
+
+		$to='vlokesh430@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name;
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
